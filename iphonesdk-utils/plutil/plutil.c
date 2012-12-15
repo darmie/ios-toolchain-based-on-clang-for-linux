77a855e916d21b27f89c326ea2bc36d46ba50122
cjacker <cjacker@gmail.com>
2012-12-15 18:31:56 +0000

    make plutil more smart

--- iphonesdk-utils/plutil/plutil.c
+++ iphonesdk-utils/plutil/plutil.c
@@ -1,164 +1,179 @@
-/*
- * plutil.C
- * source for plist convertion tool
- *
- * Copyright (c) 2008 Zach C. All Rights Reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xmlerror.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#ifndef PATH_MAX
+#include <linux/limits.h>
+#endif
 
 
 #include "plist/plist.h"
 #include "plutil.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
 
-#ifdef _MSC_VER
-#pragma warning(disable:4996)
-#endif
+int willcompile = -1;
 
+void usage() {
+  printf("plist compiler/decompiler utility\n\n");
+  printf("usage:\n");
+  printf("-c <plists>: convert xml plist to binary format\n");
+  printf("-d <plists>: convert binary plist to xml format\n");
+  printf("-h         : show this message\n");
+  printf("<plists>   : convert plist automatically\n");
+  exit(0);
+}
 
-int main(int argc, char *argv[])
+void _nullGenericErrorFunc(void* ctxt, char* msg, ...)
 {
-    FILE *iplist = NULL;
-    plist_t root_node = NULL;
-    char *plist_out = NULL;
-    uint32_t size = 0;
-    int read_size = 0;
-    char *plist_entire = NULL;
-    struct stat *filestats = (struct stat *) malloc(sizeof(struct stat));
-    Options *options = parse_arguments(argc, argv);
+  //disable all warnings and errors of libxml2.
+}
 
-    if (!options)
-    {
-        print_usage();
-        free(filestats);
-        return 0;
-    }
-    //read input file
-    iplist = fopen(options->in_file, "rb");
-    if (!iplist)
-        return 1;
-    stat(options->in_file, filestats);
-    plist_entire = (char *) malloc(sizeof(char) * (filestats->st_size + 1));
-    read_size = fread(plist_entire, sizeof(char), filestats->st_size, iplist);
-    fclose(iplist);
-
-
-    //convert one format to another
-
-
-    if (memcmp(plist_entire, "bplist00", 8) == 0)
-    {
-        plist_from_bin(plist_entire, read_size, &root_node);
-        plist_to_xml(root_node, &plist_out, &size);
-    }
-    else
-    {
-        plist_from_xml(plist_entire, read_size, &root_node);
-        plist_to_bin(root_node, &plist_out, &size);
-    }
-    plist_free(root_node);
-    free(plist_entire);
-    free(filestats);
-
-    if (plist_out)
-    {
-        if (options->out_file != NULL)
-        {
-            FILE *oplist = fopen(options->out_file, "wb");
-            if (!oplist)
-                return 1;
-            fwrite(plist_out, size, sizeof(char), oplist);
-            fclose(oplist);
-        }
-        //if no output file specified, write to stdout
-        else
-            fwrite(plist_out, size, sizeof(char), stdout);
-
-        free(plist_out);
-    }
-    else
-        printf("ERROR\n");
-
-    free(options);
+int is_xml_plist(char *plistfile)
+{
+  if(access(plistfile, R_OK) != 0)
     return 0;
+  xmlSetGenericErrorFunc(NULL, (xmlGenericErrorFunc)_nullGenericErrorFunc);
+
+  xmlDocPtr doc;
+  xmlNodePtr cur;
+  doc = xmlReadFile(plistfile, "UTF-8", 0);
+  if (doc == NULL)
+    return 0;
+
+  cur = xmlDocGetRootElement(doc);
+  if(cur == NULL) {
+    xmlFreeDoc(doc);
+    return 0;
+  }
+  if (xmlStrcmp(cur->name, (const xmlChar *) "plist")) {
+    xmlFreeDoc(doc);
+    return 0;
+  }
+  return 1;
 }
 
-Options *parse_arguments(int argc, char *argv[])
+int is_binary_plist(char *plistfile)
 {
-    int i = 0;
-
-    Options *options = (Options *) malloc(sizeof(Options));
-    memset(options, 0, sizeof(Options));
-
-    for (i = 1; i < argc; i++)
-    {
-        if (!strcmp(argv[i], "--infile") || !strcmp(argv[i], "-i"))
-        {
-            if ((i + 1) == argc)
-            {
-                free(options);
-                return NULL;
-            }
-            options->in_file = argv[i + 1];
-            i++;
-            continue;
-        }
-
-        if (!strcmp(argv[i], "--outfile") || !strcmp(argv[i], "-o"))
-        {
-            if ((i + 1) == argc)
-            {
-                free(options);
-                return NULL;
-            }
-            options->out_file = argv[i + 1];
-            i++;
-            continue;
-        }
-
-        if (!strcmp(argv[i], "--debug") || !strcmp(argv[i], "-d") || !strcmp(argv[i], "-v"))
-        {
-            options->debug = 1;
-        }
-
-        if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-h"))
-        {
-            free(options);
-            return NULL;
-        }
-    }
-
-    if (!options->in_file /*|| !options->out_file */ )
-    {
-        free(options);
-        return NULL;
-    }
-
-    return options;
+  if(access(plistfile, R_OK) != 0)
+    return 0;
+  char *plist_entire = NULL;
+  FILE *fp = fopen(plistfile, "rb");
+  if(!fp)
+    return 0;
+  struct stat st;
+  stat(plistfile, &st);
+  plist_entire = (char *) malloc(sizeof(char) * (st.st_size + 1));
+  int read_size = fread(plist_entire, sizeof(char), st.st_size, fp);
+  fclose(fp);
+  if (memcmp(plist_entire, "bplist00", 8) == 0) {
+    free(plist_entire);
+    return 1;
+  }
+  free(plist_entire);
+  return 0; 
 }
 
-void print_usage()
+int is_plist(char *plistfile)
 {
-    printf("Usage: plistutil -i|--infile in_file.plist -o|--outfile out_file.plist [--debug]\n");
-    printf("\n");
-    printf("\t-i or --infile: The file to read in.\n");
-    printf("\t-o or --outfile: The file to convert to.\n");
-    printf("\t-d, -v or --debug: Provide extended debug information.\n\n");
+  return (is_binary_plist(plistfile) || is_xml_plist(plistfile));
+}
+
+void compile_stub(char *plistfile, int compile)
+{
+  if(!plistfile)
+    return;  
+  FILE *file_in = fopen(plistfile, "rb");
+  if(!file_in)
+    return;
+  struct stat st;
+  stat(plistfile, &st);
+  char * plist_entire = (char *) malloc(sizeof(char) * (st.st_size + 1));
+  int read_size = fread(plist_entire, sizeof(char), st.st_size, file_in);
+  fclose(file_in);
+ 
+  plist_t root_node = NULL;
+  char *plist_out = NULL;
+
+  int size;
+  if(compile) {
+    plist_from_xml(plist_entire, read_size, &root_node);
+    plist_to_bin(root_node, &plist_out, &size);
+  } else { 
+    plist_from_bin(plist_entire, read_size, &root_node);
+    plist_to_xml(root_node, &plist_out, &size);
+  }
+  plist_free(root_node);
+  free(plist_entire);
+
+  char * outfilename = (char *)malloc(PATH_MAX);
+  sprintf(outfilename, "%s.tmp", plistfile);
+  if (plist_out) {
+    FILE *oplist = fopen(outfilename, "wb");
+    if(!oplist)
+      return;
+    fwrite(plist_out, size, sizeof(char), oplist);
+    fclose(oplist);
+    free(plist_out);
+  }
+  unlink(plistfile);
+  rename(outfilename, plistfile);
+}
+
+void decompile(char *plistfile)
+{
+  compile_stub(plistfile, 0);
+} 
+
+void compile(char *plistfile)
+{
+  compile_stub(plistfile, 1);
+}
+
+int main(int argc, char **argv)
+{
+  char *cmd_name = argv[0];
+  if(argc == 1)
+    usage();
+
+  char optflag;
+  while((optflag = getopt(argc, argv, "hcd")) != -1) {
+    switch(optflag) {
+    case 'c':
+      willcompile = 1;
+      break;
+    case 'd':
+      willcompile = 0;
+      break;
+    case 'h':
+    default:
+      usage();
+    }
+  }
+
+  argc = argc - optind;
+  argv = argv + optind;
+  int i;
+  for(i = 0; i < argc; i++) {
+    if(willcompile == 1 && is_binary_plist(argv[i]) == 0) { 
+      compile(argv[i]);
+    }
+    else if(willcompile == 0 && is_binary_plist(argv[i]) == 1) { 
+      decompile(argv[i]);
+    }
+    else if(willcompile == -1 && is_plist(argv[i])){
+      if(is_xml_plist(argv[i]))
+        compile(argv[i]);
+      else
+        decompile(argv[i]);
+    }
+    else 
+        continue;
+  }
+  return 0;
 }
